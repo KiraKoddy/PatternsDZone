06678dd80ba3d8e2d14c1a492fc2439dea726289
KiraKoddy <flatcher2007@narod.ru>
2022-09-19 19:49:45 +0300

    upd

--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -1,5 +1,7 @@
 package ru.netology.test;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.netology.main.DataGenerator;
 
@@ -10,12 +12,16 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
+ class CardDeliveryTest {
 
-public class CardDeliveryTest {
+     @BeforeEach
+     void setup() {
+         open("http://localhost:9999");
+     }
 
     @Test
-    void shouldBeAccepted() {
-        open("http://localhost:9999");
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldSuccessfulPlanAndReplanMeeting() {
         var validUser = DataGenerator.Registration.generateUser("ru");
         var daysToAddForFirstMeeting = 4;
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
