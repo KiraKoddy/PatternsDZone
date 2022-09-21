69ea01d8578ffa78139de0dcb1b56d971ca813c5
KiraKoddy <flatcher2007@narod.ru>
2022-09-21 13:19:59 +0300

    fix

--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -1,45 +1,40 @@
 package ru.netology.test;
 
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.netology.main.DataGenerator;
-
-import java.time.Duration;
-
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
- class CardDeliveryTest {
+class CardDeliveryTest {
 
-     @BeforeEach
-     void setup() {
-         open("http://localhost:9999");
-     }
-
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
     @Test
     @DisplayName("Should successful plan and replan meeting")
     void shouldSuccessfulPlanAndReplanMeeting() {
-        var validUser = DataGenerator.Registration.generateUser("ru");
-        var daysToAddForFirstMeeting = 4;
-        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
-        var daysToAddForSecondMeeting = 7;
-        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-        $("[data-test-id=city] .input__control").setValue(validUser.getCity());
-        $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id=date] input").setValue(firstMeetingDate);
-        $("[data-test-id=name] .input__control").setValue(validUser.getName());
-        $("[data-test-id=phone] .input__control").setValue(validUser.getPhone());
-        $(("[data-test-id=agreement]")).click();
-        $(byText("Запланировать")).click();
-        $(".notification").shouldBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id='date'] input").doubleClick();
-        $("[data-test-id='date'] input").setValue(secondMeetingDate);
-        $(byText("Запланировать")).click();
-        $(byText("Перепланировать")).click();
-        $(".notification").shouldBe(visible, Duration.ofSeconds(15));
+    var validUser = DataGenerator.Registration.generateUser("ru");
+    var daysToAddForFirstMeeting = 4;
+    var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+    var daysToAddForSecondMeeting = 7;
+    var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+    $("[data-test-id=city] input").setValue(validUser.getCity());
+    $("[data-test-id=date] .input__control").doubleClick().sendKeys(firstMeetingDate);
+    $("[data-test-id=name] input").setValue(validUser.getName());
+    $("[data-test-id=phone] input").setValue(validUser.getPhone());
+    $("[data-test-id=agreement]").click();
+    $(".button").click();
+    $("[data-test-id=success-notification] .notification__content")
+            .shouldBe(visible).should(exactText("Встреча успешно запланирована на " + firstMeetingDate));
+    $("[data-test-id=date] .input__control").doubleClick().sendKeys(secondMeetingDate);
+    $(".button").click();
+    $$(".button").find(exactText("Перепланировать")).click();
+    $("[data-test-id=success-notification] .notification__content")
+            .shouldBe(visible).should(exactText("Встреча успешно запланирована на " + secondMeetingDate));
     }
 }
-
